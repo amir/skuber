be90916257e732788c5ee655d51e0b487e0c6d63
Amir Saeid <amir@glgdgt.com>
2018-12-03 23:03:05 +0000

    Use Reflectors as Source

--- examples/src/main/scala/skuber/examples/alpha/controller/WithTypeCase.scala
+++ examples/src/main/scala/skuber/examples/alpha/controller/WithTypeCase.scala
@@ -3,11 +3,12 @@ package skuber.examples.alpha.controller
 import akka.actor.ActorSystem
 import akka.http.scaladsl.model.StatusCodes
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.{Merge, Sink, Source}
 import play.api.libs.json.Format
 import shapeless._
 import skuber.LabelSelector.IsEqualRequirement
 import skuber._
+import skuber.api.alpha.controller._
 import skuber.json.format._
 import skuber.json.ext.format._
 import skuber.api.alpha.controller.ListWatch.ListWatchers
@@ -17,8 +18,6 @@ import skuber.ext.Deployment
 import scala.concurrent.Future
 
 object WithTypeCase extends App {
-
-  import skuber.api.client.EventType
 
   implicit val system = ActorSystem()
   implicit val dispatcher = system.dispatcher
@@ -35,7 +34,7 @@ object WithTypeCase extends App {
   val foosListWatcher = new ListWatchers[Foo](k8s)
 
   def getInNamespaceOption[O <: ObjectResource](name: String, namespace: String)(
-                                               implicit fmt: Format[O], rd: ResourceDefinition[O], lc: LoggingContext
+    implicit fmt: Format[O], rd: ResourceDefinition[O], lc: LoggingContext
   ): Future[Option[O]] = {
     k8s.getInNamespace[O](name, namespace).map { result =>
       Some(result)
@@ -50,18 +49,19 @@ object WithTypeCase extends App {
       "controller" -> f.metadata.name
     )
 
-    val container = Container(name = "nginx", image="nginx:latest")
+    val container = Container(name = "nginx", image = "nginx:latest")
 
-    val template = Pod.Template.Spec(metadata=ObjectMeta()).addLabels(labels).addContainer(container)
+    val template = Pod.Template.Spec(metadata = ObjectMeta()).addLabels(labels).addContainer(container)
 
     val deployment = Deployment(metadata = ObjectMeta(
       name = f.spec.fold("foo")(_.deploymentName),
       namespace = f.metadata.namespace,
       ownerReferences = List(OwnerReference(
-        apiVersion = f.apiVersion, kind = f.kind, name = f.metadata.name, uid = "1", controller = None, blockOwnerDeletion = None
+        apiVersion = f.apiVersion, kind = f.kind, name = f.metadata.name, uid = f.uid,
+        controller = Some(true), blockOwnerDeletion = Some(true)
       ))
     )).withReplicas(f.spec.fold(1)(_.replicas))
-      .withLabelSelector(LabelSelector(labels.map(x => IsEqualRequirement(x._1, x._2)).toList :_*))
+      .withLabelSelector(LabelSelector(labels.map(x => IsEqualRequirement(x._1, x._2)).toList: _*))
       .withTemplate(template)
 
     k8s.create[Deployment](deployment)
@@ -69,39 +69,41 @@ object WithTypeCase extends App {
 
   def updateDeployment(f: Foo, deployment: Deployment): Future[Deployment] = {
     val updated = f.spec.fold(deployment) { spec =>
-      deployment.withResourceVersion(deployment.metadata.resourceVersion)
-        .copy(metadata = deployment.metadata.copy(name=spec.deploymentName))
+      deployment.withResourceVersion(f.metadata.resourceVersion)
+        .copy(metadata = deployment.metadata.copy(name = spec.deploymentName))
         .withReplicas(spec.replicas)
     }
 
     k8s.update[Deployment](updated)
   }
 
-  def reconcile[O <: ObjectResource](o: K8SWatchEvent[O]): ObjectResource = {
+  def reconcile(o: ObjectResource): ObjectResource = {
     o match {
-      case `K8SWatchEvent[Foo]`(we) =>
-        we._object.spec.foreach { spec =>
-          getInNamespaceOption[Deployment](name = spec.deploymentName, namespace = we._object.metadata.namespace).map {
-            case Some(d) =>
-              updateDeployment(we._object, d)
-            case None =>
-              createDeployment(we._object)
+      case f: Foo =>
+        f.spec.foreach { spec =>
+          getInNamespaceOption[Deployment](name = spec.deploymentName, namespace = f.metadata.namespace).map {
+            case Some(d) => updateDeployment(f, d)
+            case None => createDeployment(f)
           }
         }
-        we._object
-      case `K8SWatchEvent[Deployment]`(we) => we._object
+
+        f
+      case _ => o
     }
   }
 
-  podsListWatcher.listWatchAll.watch(None)
-    .merge(deploymentsListWatcher.listWatchAll.watch(None))
-      .merge(foosListWatcher.listWatchAll.watch(None)
-  ).map { we =>
-    we._type match {
-      case EventType.ADDED => reconcile(we)
-      case EventType.MODIFIED => reconcile(we)
-      case EventType.DELETED => we
-      case EventType.ERROR => we
-    }
+  val podsReflector = new Reflector(podsListWatcher.listWatchAll)
+  val deploymentsReflector = new Reflector(deploymentsListWatcher.listWatchAll)
+  val foosReflector = new Reflector(foosListWatcher.listWatchAll)
+
+  val podsCommands = Source.single(Reflector.DoReflect).via(podsReflector.commandFlow)
+  val deploymentCommands = Source.single(Reflector.DoReflect).via(deploymentsReflector.commandFlow)
+  val foosCommands = Source.single(Reflector.DoReflect).via(foosReflector.commandFlow)
+
+  Source.combine(podsCommands, deploymentCommands, foosCommands)(Merge(_)).map {
+    case Replacement(_, o) =>
+      reconcile(o)
+    case Updated(o) => reconcile(o)
+    case _ =>
   }.runWith(Sink.foreach(println))
 }
